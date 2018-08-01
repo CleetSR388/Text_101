407c4c3bd85c0a6bba5cab512668b156e5e7327f
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-08-01 14:54:42 -0400

    Added screen code for first state

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+
+    // Use this for initialization
+	void Start ()
+    {
+        textComponent.text = ("Your a space marine and" +
+"you've lost all contact with your team and need to find them!" +
+"You are alone. After beaming in, to arrive at your current location."
++ "Your inside a structure doors all around you." + 
+"Clearly you can see out the window it appears you are possibly on Mars" + 
+"Your options are");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
