d4dfa65c595c3202dc6f80ecf012ca85d51b53de
CleetSR388 <41758594+CleetSR388@users.noreply.github.com>
2018-08-02 02:53:20 -0400

    Added all states no q function yet one typo to correct

---
+++ Text101/Assets/RiddleResponse.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    public string stringToEdit = "What is your answer? ";
+
+    void OnGUI()
+    {
+        // Make a text field that modifies stringToEdit.
+        stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+    }
+}
